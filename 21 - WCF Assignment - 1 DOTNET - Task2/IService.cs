8ce7c8f57d7b65203aaf4b6ee17ca57f13bcd5d8
nagarajapm <43174413+nagarajapm@users.noreply.github.com>
2018-11-13 22:12:57 +0530

    Add files via upload

---
+++ 21 - WCF Assignment - 1 DOTNET - Task2/IService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _21___WCF_Assignment___1_DOTNET___Task2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService
+    {
+
+        [OperationContract]
+        List<string> OpeningJobs();
+
+        [OperationContract]
+        List<string> OpeningJobsByRole(string Role);
+
+        // TODO: Add your service operations here
+    }
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Roles
+    {
+        string pRoleName = "";
+        string pJobName = "";
+        [DataMember]
+        public string RoleName
+        {
+            get
+            {
+                return pRoleName;
+            }
+            set
+            {
+                pRoleName = value;
+            }
+        }
+
+        [DataMember]
+        public string JobName
+        {
+            get
+            {
+                return pJobName;
+            }
+            set
+            {
+                pJobName = value;
+            }
+        }
+    }
+}
---
+++ 21 - WCF Assignment - 1 DOTNET - Task2/Service.svc.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _21___WCF_Assignment___1_DOTNET___Task2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+        public List<string> OpeningJobs()
+        {
+            List<string> pRoles = GetJobs("");
+            return pRoles;
+        }
+
+        public List<string> OpeningJobsByRole(string Role)
+        {
+            List<string> pRoles = GetJobs(Role);
+            return pRoles;
+        }
+
+        private List<Roles> ListofRoles()
+        {
+            List<Roles> pRoles = new List<Roles>();
+
+            Roles pRole3 = new Roles();
+            pRole3.JobName = "Trainee";
+            pRole3.RoleName = "User";
+            pRoles.Add(pRole3);
+
+            Roles pRole4 = new Roles();
+            pRole4.JobName = "Programmer Analyst";
+            pRole4.RoleName = "Admin";
+            pRoles.Add(pRole4);
+
+
+            Roles pRole1 = new Roles();
+            pRole1.JobName = "Associate";
+            pRole1.RoleName = "Admin";
+            pRoles.Add(pRole1);
+
+            Roles pRole2 = new Roles();
+            pRole2.JobName = "Senior Associate";
+            pRole2.RoleName = "Super Admin";
+            pRoles.Add(pRole2);
+
+
+            Roles pRole5 = new Roles();
+            pRole5.JobName = "Manager";
+            pRole5.RoleName = "Super Admin";
+            pRoles.Add(pRole1);
+            return pRoles;
+        }
+        private List<string> GetJobs(string role)
+        {
+            List<Roles> proles = ListofRoles();
+            List<string> jobs = new List<string>();
+            if (role != "")
+            {
+                jobs = (from Role in proles
+                        where Role.RoleName == role
+                        select Role.JobName).ToList();
+            }
+            else
+            {
+                jobs = (from Role in proles
+                        select Role.JobName).ToList();
+            }
+            return jobs;
+        }
+    }
+}
